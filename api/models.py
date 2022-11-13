8753a9439132a07ec4d91f43e47e92d65081c83a
juandavidtb02 <jdtorres@unillanos.edu.co>
2022-11-13 02:10:34 -0500

    project created

---
+++ api/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class Categories(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'categories'
+
+
+class Colors(models.Model):
+    color_id = models.CharField(primary_key=True, max_length=15)
+    color_name = models.CharField(max_length=25)
+    color_category = models.CharField(max_length=25)
+
+    class Meta:
+        managed = False
+        db_table = 'colors'
+
+
+class Deliveries(models.Model):
+    delivery_id = models.AutoField(primary_key=True)
+    delivery_date = models.DateField()
+    delivery_price = models.IntegerField()
+    delivery_status = models.CharField(max_length=12)
+    delivery_descrip = models.CharField(max_length=280, blank=True, null=True)
+    order = models.ForeignKey('Orders', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'deliveries'
+
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_date = models.DateField()
+    order_status = models.CharField(max_length=12)
+    order_ammount = models.IntegerField()
+    userid = models.ForeignKey('Users', models.DO_NOTHING, db_column='userid')
+
+    class Meta:
+        managed = False
+        db_table = 'orders'
+
+
+class ProductColor(models.Model):
+    product = models.OneToOneField('Products', models.DO_NOTHING, primary_key=True)
+    color = models.ForeignKey(Colors, models.DO_NOTHING)
+    color_stock = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'product_color'
+        unique_together = (('product', 'color'),)
+
+
+class Products(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=30)
+    product_price = models.IntegerField()
+    product_stock = models.IntegerField()
+    product_image = models.CharField(max_length=200, blank=True, null=True)
+    product_descrip = models.CharField(max_length=300, blank=True, null=True)
+    category = models.ForeignKey(Categories, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'products'
+
+
+class Sales(models.Model):
+    product = models.OneToOneField(Products, models.DO_NOTHING, primary_key=True)
+    order = models.ForeignKey(Orders, models.DO_NOTHING)
+    sale_quantity = models.SmallIntegerField()
+    sale_price = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'sales'
+        unique_together = (('product', 'order'),)
+
+
+class Users(AbstractUser):
+    userid = models.AutoField(primary_key=True)
+    usermail = models.CharField(max_length=30,unique=True)
+    username = models.CharField(max_length=30)
+    password_user = models.CharField(max_length=256)
+    userphone = models.CharField(max_length=12, blank=True, null=True)
+    userrole = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'usermail'
+    REQUIRED_FIELDS = []
+
+
+class Wishes(models.Model):
+    product = models.OneToOneField(Products, models.DO_NOTHING, primary_key=True)
+    userid = models.ForeignKey(Users, models.DO_NOTHING, db_column='userid')
+    whis_date = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'wishes'
+        unique_together = (('product', 'userid'),)
---
+++ api/serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Users
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['userid','usermail','username','password_user','userphone','userrole']
+        extra_kwargs = {
+            'passsword_user':{'write_only':True}
+        }
+
+
+    def create(self,validated_data):
+        password = validated_data.pop('password_user',None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
